dcb8ed95ab79e76e915dfc5e61452d888f139d4d
Prajwal B S <prajwaljhon@gmail.com>
2024-08-13 19:57:47 +0530

    modified listgroup.tsx

--- my app/src/components/listgroup.tsx
+++ my app/src/components/listgroup.tsx
@@ -1,13 +1,20 @@
 import { Fragment } from "react/jsx-runtime";
 
 export function ListGroup() {
-const a=["Amin","Jhon","Jarvis","Friday"]
+let a=["Amin","Jhon","Jarvis","Friday"]
+a=[]
+// const getmsg = ()=>{
+//     return a.length===0 ?<p>No Data Found</p>: null
+
+// }
 
 
     return (<Fragment><h1>List Group</h1>
+    {a.length===0 && <p>No Items Found At 38:29</p>}
         <ul className="list-group">
-        {a.map(name => <li>{name}</li>)}
+        {a.map(name => <li key={name}>{name}</li>)}
     </ul></Fragment>);
 }
 
 //export default listgroup
+ 
