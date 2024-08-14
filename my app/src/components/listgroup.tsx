47a3ef213668fbd7b9bc7523e682bae12b9762a8
Prajwal B S <prajwaljhon@gmail.com>
2024-08-14 12:29:53 +0530

    onclick function added

--- my app/src/components/listgroup.tsx
+++ my app/src/components/listgroup.tsx
@@ -1,20 +1,24 @@
 import { Fragment } from "react/jsx-runtime";
+import { MouseEvent } from "react";
 
 export function ListGroup() {
-let a=["Amin","Jhon","Jarvis","Friday"]
-a=[]
-// const getmsg = ()=>{
-//     return a.length===0 ?<p>No Data Found</p>: null
+    const a = ["Amin", "Jhon", "Jarvis", "Friday"]
+    //a=[]
+    // const getmsg = ()=>{
+    //     return a.length===0 ?<p>No Data Found</p>: null
 
-// }
+    // }
+    //writing function outtside of elements because of it's logics
+    const handle=(e:MouseEvent)=>{
+        console.log(e.currentTarget)
+    }
 
 
     return (<Fragment><h1>List Group</h1>
-    {a.length===0 && <p>No Items Found At 38:29</p>}
+        {a.length === 0 && <p>No Items Found At 38:29</p>}
         <ul className="list-group">
-        {a.map(name => <li key={name}>{name}</li>)}
-    </ul></Fragment>);
+            {a.map(name => <li className="list-group-item" onClick={handle} key={name}>{name}</li>)}
+        </ul></Fragment>);
 }
 
 //export default listgroup
- 
