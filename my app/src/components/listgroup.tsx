25539086455a850dfb1e2c9c5bd8399eda70aed8
Prajwal B S <prajwaljhon@gmail.com>
2024-08-16 10:55:46 +0530

    removed props and added {name,heading }:Props(Properties)

--- my app/src/components/listgroup.tsx
+++ my app/src/components/listgroup.tsx
@@ -6,7 +6,7 @@ interface Props{
 
 }
 
-export function ListGroup(props:Props) {
+export function ListGroup({name,heading}:Props) {
     
     const [selectedind,setselectedind]=useState(-1)
     //a=[]
@@ -20,10 +20,10 @@ export function ListGroup(props:Props) {
     // }
 
 
-    return (<Fragment><h1>{props.heading}</h1>
-        {props.name.length === 0 && <p>No Items Found At 38:29</p>}
+    return (<Fragment><h1>{heading}</h1>
+        {name.length === 0 && <p>No Items Found At 38:29</p>}
         <ul className="list-group">
-            {props.name.map((name,index) => <li className={selectedind==index ? "list-group-item active":"list-group-item"} key={name} onClick={() =>{setselectedind(index);} }>{name}</li>)}
+            {name.map((name,index) => <li className={selectedind==index ? "list-group-item active":"list-group-item"} key={name} onClick={() =>{setselectedind(index);} }>{name}</li>)}
         </ul></Fragment>);
 }
 
